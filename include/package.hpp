bc2d9b28bdb619d3fc350de0c94768e53460f54e
Julita Wójcik <julitaw@student.agh.edu.pl>
2021-12-19 16:56:32 +0100

    coś

--- include/package.hpp
+++ include/package.hpp
@@ -4,5 +4,29 @@
 
 #ifndef NET_SIM_PACKAGE_HPP
 #define NET_SIM_PACKAGE_HPP
+#include <set>
+
+using ElementID = int;
+
+class Package
+{
+
+
+public:
+    Package();
+    Package(ElementID id);
+
+    bool operator==(const Package& p);
+    ElementID get_id();
+
+    ~Package();
+
+private:
+    static std::set<ElementID> freed_IDs_;
+    static std::set<ElementID> assigned_IDs_;
+    ElementID id;
+
+};
+
 
 #endif //NET_SIM_PACKAGE_HPP
