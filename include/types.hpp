d39364f820ae957f109a9f9fdbd124ddfc63a039
zojapila <80410917+zojapila@users.noreply.github.com>
2022-01-02 23:26:13 +0100

    aliasy do wezlow

--- include/types.hpp
+++ include/types.hpp
@@ -5,7 +5,20 @@
 #ifndef NET_SIM_TYPES_HPP
 #define NET_SIM_TYPES_HPP
 
-using ElementID = unsigned int;
+#include <random>
+#include <ctime>
+#include <functional>
 
+//std::random_device rd;
+//std::mt19937 rng(rd());
+
+//double probability_generator_1() {
+//    return std::generate_canonical<double, 10>(rng);
+//}
+
+using ElementID = unsigned int;
+using Time = unsigned int;
+using TimeOffset = unsigned int;
+using ProbabilityGenerator = std::function<double> ;
 
 #endif //NET_SIM_TYPES_HPP
