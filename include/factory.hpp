ffb9846d44255a2d05fff220832ea25a60227708
marcjannabakowska <81325217+marcjannabakowska@users.noreply.github.com>
2022-01-11 11:29:59 +0100

    twoja stara ma komara

--- include/factory.hpp
+++ include/factory.hpp
@@ -9,12 +9,22 @@
 #include "nodes.hpp"
 #include <vector>
 
+
+enum NodeColor { UNVISITED, VISITED, VERIFIED };
+bool has_reachable_storehouse(const PackageSender* sender, std::map<const PackageSender*, NodeColor>& node_colors);
+
+
 template <class Node>
 class NodeCollection {
 public:
     using container_t = typename std::vector<Node>;
     using iterator = typename container_t::iterator;
     using const_iterator = typename container_t::const_iterator;
+
+    iterator begin() { return container.begin(); }
+    iterator end() { return container.end(); }
+    const_iterator cbegin() const { return container.cbegin(); }
+    const_iterator cend() const { return container.cend(); }
 
 
 private:
@@ -30,7 +40,8 @@ public:
 };
 class Factory {
 private:
-    void remove_receiver(NodeCollection<Node>::const_iterator& collection, ElementID id) =0;
+    template<class Node>
+    void remove_receiver(NodeCollection<Ramp>::const_iterator& collection, ElementID id);
     NodeCollection<Ramp> cont_r;
     NodeCollection<Worker> cont_w;
     NodeCollection<Storehouse> cont_s;
@@ -44,27 +55,26 @@ public:
     NodeCollection<Ramp>::const_iterator  ramp_cend() const {return cont_r.cend();}
 
 
-    void add_worker(Worker&& w) {cont_w.add(std::move(w))};
-    void remove_worker(ElementID id) {cont_w.remove_by_id(id);};
+    void add_worker(Worker&& w) {cont_w.add(std::move(w));}
+    void remove_worker(ElementID id) {cont_w.remove_by_id(id);}
     NodeCollection<Worker>::iterator find_worker_by_id(ElementID id) {return cont_w.find_by_id(id);}
     NodeCollection<Worker>::const_iterator find_worker_by_id(ElementID id) const {return cont_w.find_by_id(id);}
     NodeCollection<Worker>::const_iterator worker_cbegin() const {return cont_w.cbegin();}
     NodeCollection<Worker>::const_iterator  worker_cend() const {return cont_w.cend();}
 
 
-    void add_storehouse(Storehouse&& s) {cont_s.add(std::move(r))};
-    void remove_storehouse(ElementID id) {cont_s.remove_by_id(id);};
+    void add_storehouse(Storehouse&& s) {cont_s.add(std::move(s));}
+    void remove_storehouse(ElementID id) {cont_s.remove_by_id(id);}
     NodeCollection<Storehouse>::iterator find_storehouse_by_id(ElementID id) {return cont_s.find_by_id(id);}
     NodeCollection<Storehouse>::const_iterator find_storehouse_by_id(ElementID id) const {return cont_s.find_by_id(id);}
     NodeCollection<Storehouse>::const_iterator storehouse_cbegin() const {return cont_s.cbegin();}
     NodeCollection<Storehouse>::const_iterator  storehouse_cend() const {return cont_s.cend();}
 
-    bool is_consistent()=0; //todo
-    void do_deliveries(Time)=0;//todo
-    void do_package_passing()=0;//todo
-    void do_work(Time)=0; //todo
+    bool is_consistent();
+    void do_deliveries(Time);
+    void do_package_passing();
+    void do_work(Time);
 
 };
-
 
 #endif //NET_SIM_FACTORY_HPP
--- src/factory.cpp
+++ src/factory.cpp
@@ -2,3 +2,118 @@
 // Created by ZoJa on 14.12.2021.
 //
 
+#include "factory.hpp"
+
+
+bool has_reachable_storehouse(/* const */PackageSender* sender, std::map<const PackageSender*, NodeColor>& node_colors){
+    if(node_colors[sender] == NodeColor::VERIFIED){
+        return true;
+    }
+    node_colors[sender] = NodeColor::VISITED;
+    if(sender->receiver_preferences_.get_preferences().empty()){
+        throw std::logic_error("Sender has not got any receivers");
+    }
+    bool has_sender_at_least_one_receiver_other_than_himself = false;
+    for(auto receiver : sender->receiver_preferences_.get_preferences()){
+        if(receiver.first->get_receiver_type() == ReceiverType::STOREHOUSE){
+            has_sender_at_least_one_receiver_other_than_himself = true;
+        }
+        else if(receiver.first->get_receiver_type() == ReceiverType::WORKER){
+            IPackageReceiver* receiver_ptr = receiver.first;
+            auto worker_ptr = dynamic_cast<class Worker*>(receiver_ptr);
+            auto sendrecv_ptr = dynamic_cast<PackageSender*>(worker_ptr);
+            if(sendrecv_ptr == sender){
+                continue;
+            }
+            has_sender_at_least_one_receiver_other_than_himself = true;
+            if(node_colors[sendrecv_ptr] == NodeColor::UNVISITED){
+                has_reachable_storehouse(sendrecv_ptr,node_colors);
+            }
+        }
+    }
+    node_colors[sender] = NodeColor::VERIFIED;
+    if(has_sender_at_least_one_receiver_other_than_himself){
+        return true;
+    }
+    else{
+        throw std::logic_error("Error");
+    }
+}
+
+void Factory::do_deliveries(Time t) {
+    for (auto &ramp : cont_r) {
+        ramp.deliver_goods(t);
+    }
+}
+
+void Factory::do_work(Time t ) {
+    for (auto& worker : cont_w)
+        worker.do_work(t);
+}
+
+void Factory::do_package_passing() {
+    for (auto &ramp : cont_r) {
+        ramp.send_package();
+    }
+    for (auto &worker : cont_w) {
+        worker.send_package();
+    }
+}
+
+bool Factory::is_consistent() {
+    std::map<const PackageSender *, NodeColor> kolor;
+    for (auto ptr = cont_w.begin(); ptr != cont_w.end(); ptr++){
+        Worker* worker = &(*ptr);
+        auto sender = dynamic_cast<PackageSender*>(worker);
+        kolor[sender] = NodeColor::UNVISITED;
+    }
+    for (auto ptr = cont_r.begin(); ptr != cont_r.end(); ptr++){
+        Ramp* ramp = &(*ptr);
+        auto sender = dynamic_cast<PackageSender*>(ramp);
+        kolor[sender] = NodeColor::UNVISITED;
+    }
+    try {
+        for (auto ptr = cont_r.begin(); ptr != cont_r.end(); ptr++){
+            Ramp* ramp = &(*ptr);
+            auto sender = dynamic_cast<PackageSender*>(ramp);
+            has_reachable_storehouse(sender, kolor);
+        }
+    }
+    catch (const std::logic_error&) {
+        return false;
+    }
+    return true;
+}
+
+template<class Node>
+void Factory::remove_receiver(NodeCollection<Ramp>::const_iterator& collection, ElementID id) {
+
+    auto iter = collection.find_by_id(id);
+    auto receiver_ptr = dynamic_cast<IPackageReceiver>(&(iter));
+
+    for(auto & ramp : cont_r)
+    {
+        auto & _preferences = ramp.receiverpreferences.get_preferences();
+        for(auto _preference : _preferences)
+        {
+            if(_preferences.first == receiver_ptr)
+            {
+                ramp.receiverpreferences.remove_receiver(receiver_ptr);
+                break;
+            }
+        }
+    }
+
+    for(auto & worker : cont_w)
+    {
+        auto & _preferences = worker.receiverpreferences.get_preferences();
+        if(auto _preference  : _preferences)
+        {
+            if(_preferences.first == receiver_ptr)
+            {
+                worker.receiverpreferences.remove_receiver(receiver_ptr);
+                break;
+            }
+        }
+    }
+}
