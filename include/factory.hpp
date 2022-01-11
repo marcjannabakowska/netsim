fc5415de2a38d3f952f055391d505b60a206c8d8
marcjannabakowska <81325217+marcjannabakowska@users.noreply.github.com>
2022-01-11 12:39:31 +0100

    nei wiem czy robie to dobrze

--- include/factory.hpp
+++ include/factory.hpp
@@ -7,7 +7,7 @@
 
 #include "storage_types.hpp"
 #include "nodes.hpp"
-#include <vector>
+#include <list>
 
 
 enum NodeColor { UNVISITED, VISITED, VERIFIED };
@@ -17,7 +17,7 @@ bool has_reachable_storehouse(const PackageSender* sender, std::map<const Packag
 template <class Node>
 class NodeCollection {
 public:
-    using container_t = typename std::vector<Node>;
+    using container_t = typename std::list<Node>;
     using iterator = typename container_t::iterator;
     using const_iterator = typename container_t::const_iterator;
 
@@ -35,17 +35,17 @@ public:
     void add(Node&& node) {container.push_back(std::move(node));}
     void remove_by_id(ElementID id)
     {
-        auto iter = std::remove_if(container.begin(), container.end(), [id](Node& elem) {return elem.get_id() == id;}); //container erase(container.begin()+id);
-        container.erase(iter);
+        container.remove_if([id](const Node& elem) {return elem.get_id() == id;}); //container erase(container.begin()+id);
+
     }
     NodeCollection<Node>::iterator find_by_id(ElementID id)
     {
-        return std::find_if(container.begin(), container.end(), [id](Node& elem) {return elem.get_id() == id;});
+        return std::find_if(container.begin(), container.end(), [id](const Node& elem) {return elem.get_id() == id;});
     }
 
     NodeCollection<Node>::const_iterator find_by_id(ElementID id) const
     {
-        return std::find_if(container.begin(), container.end(), [id](Node& elem) {return elem.get_id() == id;});
+        return std::find_if(container.begin(), container.end(), [id](const Node& elem) {return elem.get_id() == id;});
     };
 
 };
