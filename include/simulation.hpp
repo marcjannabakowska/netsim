6916b8a5203fdfd39905ce8e112b110114231f4f
zojapila <80410917+zojapila@users.noreply.github.com>
2022-01-16 22:20:17 +0100

    symulacja

--- include/simulation.hpp
+++ include/simulation.hpp
@@ -1,8 +1,13 @@
 //
-// Created by ZoJa on 14.12.2021.
+// Created by ZoJa on 16.01.2022.
 //
 
 #ifndef NET_SIM_SIMULATION_HPP
 #define NET_SIM_SIMULATION_HPP
+#include "factory.hpp"
 
+
+
+
+//void simulate(Factory& f, TimeOffset d, std::function<void (Factory&, Time)> rf);
 #endif //NET_SIM_SIMULATION_HPP
--- src/simulation.cpp
+++ src/simulation.cpp
@@ -2,3 +2,13 @@
 // Created by ZoJa on 14.12.2021.
 //
 
+#include "simulation.hpp"
+//void simulate(Factory& f, TimeOffset d, std::function<void (Factory&, Time)> rf){
+//    if (!f.is_consistent()) { throw std::logic_error("non consistent network!"); }
+//    for(TimeOffset i = 0; i<d; i++){
+//        f.do_deliveries(i);
+//        f.do_package_passing();
+//        f.do_work(i);
+//        rf(f,i);
+//    }
+//}
